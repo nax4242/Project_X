a4eadf0208ebe1c0058262e1eb0a451431124f57
Данила П <danilapuzanov48@gmail.com>
2026-09-07 19:05:20 +0300

    Исправлена формула в вычисления площади. Сторона * Высота / 2 вместо Сторона * Сторона / 2

--- Project_X/main.cpp
+++ Project_X/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-	double first_side, second_side, area_of_triangle;
+	double first_side, height, area_of_triangle;
 
 	std::cout << "Enter first side of triangle: ";
 	std::cin >> first_side;
@@ -13,17 +13,17 @@ int main() {
 		std::cin >> first_side;
 	}
 
-	std::cout << "Enter second side of triangle: ";
-	std::cin >> second_side;
+	std::cout << "Enter height of triangle: ";
+	std::cin >> height;
 
-	while (second_side <= 0) {
-		std::cout << "Incorrect input. Side length must be positive number." << std::endl;
+	while (height <= 0) {
+		std::cout << "Incorrect input. Height length must be positive number." << std::endl;
 		std::cout << "Enter first side of triangle: ";
 
-		std::cin >> second_side;
+		std::cin >> height;
 	}
 
-	area_of_triangle = first_side * second_side / 2;
+	area_of_triangle = first_side * height / 2;
 	std::cout << "Area of triangle equals " << area_of_triangle << std::endl;
 
 	return 0;
